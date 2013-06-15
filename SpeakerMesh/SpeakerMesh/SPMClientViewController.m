609146f71212bb78adebbdbe20eb51f46531ae40
Curtis Herbert <me@curtisherbert.com>
2013-06-15 16:27:32 -0400

    Client code - maybe?

--- SpeakerMesh/SpeakerMesh/SPMClientViewController.m
+++ SpeakerMesh/SpeakerMesh/SPMClientViewController.m
@@ -8,8 +8,12 @@
 
 #import "SPMClientViewController.h"
 #import "Constants.h"
+#import "AFJSONRequestOperation.h"
+#import "AFHTTPClient.h"
 
 @interface SPMClientViewController ()
+
+@property (strong, nonatomic) NSArray *recentBeacons;
 
 @end
 
@@ -18,17 +22,15 @@
     CLBeaconRegion *_beaconRegion;
     UIDynamicAnimator *_animator;
     BOOL _isPlaying;
+    NSTimer *_serverUpdateTimer;
 }
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     _locationManager = [[CLLocationManager alloc] init];
     _locationManager.delegate = self;
-
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:DefaultUUID];
-    _beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:[uuid UUIDString]];
-    [_locationManager startRangingBeaconsInRegion:_beaconRegion];
 
     _animator = [[UIDynamicAnimator alloc]initWithReferenceView:self.view];
 
@@ -44,9 +46,27 @@
     collisionBehavior.collisionDelegate = self;
 }
 
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    self.recentBeacons = [[NSArray alloc] init];
+    
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:DefaultUUID];
+    _beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:[uuid UUIDString]];
+    [_locationManager startRangingBeaconsInRegion:_beaconRegion];
+    
+    
+    _serverUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                          target:self
+                                                        selector:@selector(updateServer:)
+                                                        userInfo:nil
+                                                         repeats:YES];
+}
+
 - (void) viewDidDisappear:(BOOL)animated
 {
     [_locationManager stopRangingBeaconsInRegion:_beaconRegion];
+    [_serverUpdateTimer invalidate];
     _beaconRegion = nil;
     _locationManager = nil;
 }
@@ -59,9 +79,40 @@
 
 - (void) locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
 {
-    for(CLBeacon *beacon in beacons) {
+    self.recentBeacons = beacons;
+}
+
+- (void)updateServer:(NSTimeInterval)interval
+{
+    NSMutableArray *beaconsForServer = [[NSMutableArray alloc] init];
+    
+    for(CLBeacon *beacon in self.recentBeacons) {
+        NSMutableDictionary *beaconForServer = [[NSMutableDictionary alloc] init];
+        [beaconForServer setObject:@(beacon.accuracy) forKey:@"accuracy"];
+        [beaconForServer setObject:@(beacon.proximity) forKey:@"proximity"];
+        [beaconForServer setObject:beacon.major forKey:@"id"];
         
+        [beaconsForServer addObject:beaconForServer];
     }
+    
+    NSURL *url = [NSURL URLWithString:@"http://curtisherbert.com/hack/setCurrentStatus"];
+    AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:url];
+    httpClient.parameterEncoding = AFJSONParameterEncoding;
+    NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
+                            [NSNumber numberWithBool:_isPlaying], @"shouldBePlaying",
+                            beaconsForServer, @"knownSpeakers",
+                            nil];
+    NSMutableURLRequest *request = [httpClient requestWithMethod:@"POST" path:nil parameters:params];
+    AFJSONRequestOperation *operation =
+    [AFJSONRequestOperation
+     JSONRequestOperationWithRequest:request
+     success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+         NSLog(@"Updated server with beacons");
+     }
+     failure:^(NSURLRequest *request, NSHTTPURLResponse *operation, NSError *error, id JSON) {
+        NSLog(@"Error updating server");
+     }];
+    [operation start];
 }
 
 - (IBAction)performPlayPause:(id)sender
